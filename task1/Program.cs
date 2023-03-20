5cf336318c546ea4b903edaea9c0472f1c511231
pashqa <pashqevi4@gmail.com>
2023-03-20 22:27:15 +0700

    Task 1 complete

---
+++ task1/Program.cs
@@ -0,0 +1,41 @@
+﻿void FillArray(int[] array1)
+{
+    int length = array1.Length;
+    int index = 0;
+    while (index < length)
+    {
+        array1[index] = new Random().Next(100, 1000);
+        index++;
+    }
+}
+
+void PrintArray(int[] array2)
+{
+    int quantity = array2.Length;
+    int count = 0;
+    while (count < quantity)
+    {
+        Console.Write($"{array2[count]}, ");
+        count++;
+    }
+}
+
+int FindEvenNumber(int[] array3)
+{
+    int length1 = array3.Length;
+    int sum = 0;
+    for (int number = 0; number < length1; number++)
+    {
+        if (array3[number] % 2 == 0)
+
+            sum++;
+    }
+    return sum;
+}
+
+int[] array = new int[50];
+FillArray(array);
+PrintArray(array);
+int pos = FindEvenNumber(array);
+Console.WriteLine();
+Console.Write($"Total of even numbers is {pos}");
